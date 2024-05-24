ba738d1c4f8d0d6d0fa0bf7241bed03c7d831bff
RafalZmu <rafalzmu15@gmial.com>
2024-05-24 11:41:41 +0200

    Add project files.

---
+++ Rafał_Żmuda_Książka_Adresowa/Controllers/AddresBookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rafał_Żmuda_Książka_Adresowa.Controllers
+{
+    public class AddresBookController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetLastAdress()
+        {
+            return Ok("Ostatni adres");
+        }
+
+        [HttpGet]
+        public IActionResult GetByCity(string city)
+        {
+            return Ok("Adresy z miasta: " + city);
+        }
+
+        [HttpPost]
+        public IActionResult AddAdress([FromBody] string adress)
+        {
+            return Ok("Dodano adres: " + adress);
+        }
+    }
+}
